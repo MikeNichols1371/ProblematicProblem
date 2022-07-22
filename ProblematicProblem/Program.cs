f2e0636c83f56c063ddf8dc9318fedef9c9c2f35
MikeNichols1371 <mnich13711@gmail.com>
2022-07-22 12:57:23 -0400

    Finished debugging

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -16,6 +16,10 @@ namespace ProblematicProblem
             Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
             string cont = Console.ReadLine().ToLower();
             Console.WriteLine();
+            if (cont == "no")
+            {
+                return;
+            }
             Console.Write("We are going to need your information first! What is your name? ");
             string userName = Console.ReadLine().ToLower();
             Console.WriteLine();
